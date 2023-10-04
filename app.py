4bf3a7204c5c2dea2cdd48df67c4c4fb10b24c02
sanjeev1996 <sanjeevamazingworld2@gmail.com>
2023-10-04 23:24:47 +0530

    a

---
+++ app.py
@@ -0,0 +1,20 @@
+#from paragraph_line import app
+
+
+import re
+from flask import Flask, jsonify, render_template, request
+import requests
+
+app = Flask(__name__)
+
+
+print("a")
+
+@app.route('/')
+def ticket_fields():
+    #return "hello"
+    return {"1": "2"}
+
+
+if __name__ == "__main__":
+  app.run()
